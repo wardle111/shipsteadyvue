adf2674b8bdec2cad974460bf80d98f41c8104ab
wardle111 <BenjaminWardle111@gmail.com>
2022-08-19 21:27:03 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,96 @@
+
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
+  <v-app id="inspire">
+        <v-row
+      align="center"
+      justify="center"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <v-col
+        class="text-center"
+        cols="12"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+        <h1 class="text-h4 font-weight-bold mb-4">
+          ShipSteady
+        </h1>
+      </v-col>
+    </v-row>
+    <v-card
+  elevation="2"
+  outlined
+></v-card>
     <v-main>
-      <HelloWorld/>
+      <template>
+      <v-parallax
+      dark
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+       >
+        <v-responsive
+    class="overflow-y-auto"
+    max-height="400"
+  >
+    <v-responsive
+      height="200vh"
+      class="text-center pa-2"
+    >
+      <v-responsive min-height="50vh"></v-responsive>
+      <v-lazy
+        v-model="isActive"
+        :options="{
+          threshold: .5
+        }"
+        min-height="200"
+        transition="fade-transition"
+      >
+    <template>
+      <v-timeline>
+      <v-timeline-item>Onboarding</v-timeline-item>
+      <v-timeline-item class="text-right">
+        Requirements Gathering
+      </v-timeline-item>
+      <v-timeline-item>Delivery</v-timeline-item>
+    </v-timeline>
+    </template>
+      </v-lazy>
+    </v-responsive>
+  </v-responsive>
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        <h1 class="text-h4 font-weight-thin mb-4">
+          ShipSteady
+        </h1>
+      </v-col>
+    </v-row>
+      </v-parallax>
+    </template>
+    <template>
+    <v-footer
+      v-bind="localAttrs"
+      :padless="padless"
+    >
+      <v-card
+        flat
+        tile
+        width="100%"
+        class="blue lighten-1 text-center"
+      >
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Hoi and Ben</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+</template>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  export default {
+    data: () => ({ drawer: null }),
+  }
 </script>
